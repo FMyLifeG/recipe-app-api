04ed05b672132b5522a92152ed6c883c361cbfbf
FMyLife <ali.tttsouli@gmail.com>
2023-09-04 18:45:38 +0100

    change to wait for database test and linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """Susbtract x from y and return result"""
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,8 +12,7 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-     
-     
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if databse ready."""
         patched_check.return_value = True
@@ -26,10 +25,9 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test  waiting for database when getting OperationalError"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-          [OperationalError] * 3 + [True]
+            [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
- 
